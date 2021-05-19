f5f3b0cae76c1fe01a52a89476d6a81606ada5a2
smart1014 <mymira1014@gmail.com>
2021-05-20 02:37:12 +0800

    feat: api for converting temperature

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to Temperature API Home"
+
+@app.route('/api/v1/converter', methods=['POST'])
+def temperature_converter():
+    """
+    API to convert temperature from Celcius to Farenheit, and vise versa
+    Request format is JSON, and sample data format is 
+        - C to F: {"type": "C", "value": 100}
+        - F to C: {"type": "F", "value": 80}
+    Output is converted value
+    """
+    # parse json data
+    temperature_data = request.get_json()
+
+    # simple validation
+    if 'type' in temperature_data.keys() and 'value' in temperature_data.keys():
+        # get temperature value, and convert it to float
+        try:
+            t_value = float(temperature_data['value'])
+        except:
+            t_value = None
+
+        if t_value != None:
+            if temperature_data['type'].upper() == 'C':
+                # Convert from Celcius to Fahrenheit
+                fahrenheit = 9.0/5.0 * t_value + 32
+                return "{:.2f}".format(fahrenheit), 200
+            elif temperature_data['type'].upper() == 'F':
+                # Convert from Fahrenheit to Celcius
+                celsius = (t_value - 32) * 5.0/9.0
+                return "{:.2f}".format(celsius), 200
+    return 'Validation Error', 200
+
+app.run()
